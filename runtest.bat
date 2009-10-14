60d798aecb88382cb8ac760ca45fe3615d490aaf
geoffspeicher <geoffspeicher@469e59e2-98e4-11dd-87e9-a30ea5446288>
2009-10-14 18:31:14 +0000

    merge c12 from http://phpandallthat.googlecode.com/svn/trunk/antlr   by Eugeny.Yakimovitch
    bat renaming

--- runtest.bat
+++ runtest.bat
@@ -1,4 +1,4 @@
 @echo off
 set PROJ_PATH=%CD%
 
-"%PHP_PEAR_BIN_DIR%\phpunit.bat" -d include_path="%PROJ_PATH%;%PROJ_PATH%\antlr;%PHP_PEAR_BIN_DIR%\pear" --verbose test/%1
+"%PHP_PEAR_BIN_DIR%\phpunit.bat" -d include_path="%PROJ_PATH%;%PROJ_PATH%\runtime\Php;%PHP_PEAR_BIN_DIR%\pear" --verbose test/%1
