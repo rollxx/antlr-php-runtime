efd848c470bb137981c5f74eeb70289ca0dd80f8
geoffspeicher <geoffspeicher@469e59e2-98e4-11dd-87e9-a30ea5446288>
2009-10-14 17:55:47 +0000

    merge r5 from http://phpandallthat.googlecode.com/svn/trunk/antlr   by Eugeny.Yakimovitch
    generation of grammars for tests
    
    
    
    git-svn-id: https://antlrphpruntime.googlecode.com/svn/trunk@10 469e59e2-98e4-11dd-87e9-a30ea5446288

--- gentestgrammars.cmd
+++ gentestgrammars.cmd
@@ -1,4 +1,12 @@
 @echo off
 SET CLASSPATH=%CLASSPATH%;.\lib\antlr-3.1.3-php.jar;.\lib\antlr-2.7.7.jar;.\lib\antlr-runtime-3.1.3.jar;.\lib\gunit.jar;.\lib\stringtemplate-3.2.jar 
-rem java org.antlr.Tool -o test test\t001lexer.g 
-java org.antlr.Tool
+
+rem java org.antlr.Tool
+rem goto skip
+
+for %%i in (test\*.g) DO (
+	ECHO [generating %%i]
+	java org.antlr.Tool -make -fo test %%i
+)
+
+:skip
